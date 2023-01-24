2eef074251e995066e33c23e3cea429bacf40a24
DevshreeDeshmukh <96471488+DevshreeDeshmukh@users.noreply.github.com>
2023-01-24 18:28:06 +0530

    Add files via upload

---
+++ 02_Scripts/01_Basics/03_User_input.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#info
+echo "Enter a Number"
+read num
+echo "You have Enter "$num
+printf "You have Enter %d"$num
---
+++ 02_Scripts/01_Basics/04_run_other_linux_command.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "run linux Command"
+echo "Today's date is " `date`
+echo "My current Working directory is `pwd`"
